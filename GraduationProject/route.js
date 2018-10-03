1c095538093baad079aad42d8a1af5feb4c54814
52dbsgur <52dbsgur@naver.com>
2018-10-03 21:27:45 +0900

    add route

--- GraduationProject/route.js
+++ GraduationProject/route.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var fs = require('fs');
+var spawn = require("child_process").spawn;
 
 //routing
 router.get('/', function(req, res) {
@@ -40,14 +41,26 @@ router.post('/addHistory', function(req, res) {
 	
 	var jsonData = JSON.stringify(resultList, null, 2);
 	
-	try {
-		fs.writeFileSync(path.join(__dirname, "/history/", fileName + ".json"), jsonData);		
-	} catch (e) {
-		// TODO: handle exception
-		console.log("err");
-		res.end('{"error" : "작업 계획 기록 추가  실패"}');
-	}
-	res.end('{"success" : "작업 계획 기록 추가 성공"}');
+	fs.writeFile('./py/input.json', jsonData, 'utf-8', function(e){
+    if(e){
+        console.log(e);
+    }
+    else{
+        console.log('01 WRITE DONE!');
+    }
+    });
+    
+    var process = spawn('python',["writejson.py"],{cwd : './py/'});
+    console.log("pre");
+    process.stdout.on('data',function(chunk){
+        var textChunk = chunk.toString('utf8'); // buffer to string
+    });
+
+	console.log("end");
+
+    var resultJson = JSON.parse(fs.readFileSync(path.join(__dirname, "/saveresult/", "result.json"), 'utf-8'));
+    res.json(resultJson);
+    res.end();
 });
 
 router.post('/readHistoryList', function(req, res) {
